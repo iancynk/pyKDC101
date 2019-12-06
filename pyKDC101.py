a6861142bace57b5871aa4ae96d2be0aacb6823b
iancynk <ian.cynk@posteo.eu>
2019-12-06 14:02:37 +0100

    added more commands, removed old stuff

--- pyKDC101.py
+++ pyKDC101.py
@@ -59,201 +59,36 @@ def recvreply(s):
 
 
 commands = {
-    "identify":         "23 02 00 00 50 01",
-    "move_home":        "43 04 01 00 50 01",
-    "req_info":         "05 00 00 00 50 01",
-    "move_absolute":    "53 04 01 00 50 01",
-    "move_relative":    "48 04 01 00 50 01"
+    "identify":         "23 02 00 00 50 01", # flashes the screen
+    "move_home":        "43 04 01 00 50 01", # move home
+    "req_info":         "05 00 00 00 50 01", # get hardware info
+    "req_poscounter":   "11 04 01 00 50 01", # get position count
+    "req_enccounter":   "0A 04 01 00 50 01", # get enccounter count
+    "req_velparams":    "14 04 01 00 50 01", # get the velocity parameters
+    "req_genmoveparams":"3B 04 01 00 50 01", # get backlash settings
+    "req_homeparams":   "41 04 01 00 50 01", # get home parameters
+    "req_moverelparams":"46 04 01 00 50 01", # get rel movement parameters
+    "move_relative":    "48 04 01 00 50 01", # move predefined relative
+    "req_moveabsparams":"51 04 01 00 50 01", # get abs movement parameters
+    "move_absolute":    "53 04 01 00 50 01", # move predefined absolute
+    "move_stop":        "65 04 01 00 50 01", # stop movement
+    "req_jogparams":    "17 04 01 00 50 01", # get jog parameters
+    "move_jog_forward": "6A 04 01 02 50 01", # jog forward
+    "move_jog_backward":"6A 04 01 01 50 01", # jog backward
+    "move_velocity_f":  "57 04 01 02 50 01", # move at fixed speed forward
+    "move_velocity_b":  "57 04 01 01 50 01", # move at fixed speed backward
 }
 
 s = openstage()
-
 sendcommand(s, commands["identify"])
+time.sleep(0.1)
+
+# %%
+sendcommand(s, commands["move_stop"])
 reply = recvreply(s)
 
-
 # %%
-# identify the stage (flashes the display)
-# structure:
-# 23 02 = command (here: instruct hardware to identify by flashing front panel LEDs)
-# 00 00 = param1 and param2 (here zero)
-# 50 = dest (here: generic USB unit)
-# 01 = source (here: simply means host)
-string = ('23 02 00 00 50 01') # command in hex
-splitstring = string.split() # separate in to list of 6 hex values
-command = [int(str, 16) for str in splitstring] # convert to integer
-print('sending command: ', command)
-s.write(bytes(command))
-
-# %%
-# move home
-string = ('43 04 01 00 50 01')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-# %%
-# get hardware info
-string = ('05 00 00 00 50 01')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-time.sleep(0.04) # has to be at least 20 ms to work on my computer
-print('bytes in queue: ', s.in_waiting)
-hwinfo = ''
-while s.in_waiting > 0:
-    # read every single byte (converted to hex) and add whitespace
-    hwinfo += s.read().hex()
-    hwinfo += ' '
-
-print('hardware info: ', hwinfo)
-
-# %%
-# get velocity parameters
-string = ('14 04 01 00 50 01')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-time.sleep(0.04)
-print('bytes in queue: ', s.in_waiting)
-velparams = ''
-while s.in_waiting > 0:
-    velparams += s.read().hex()
-    velparams += ' '
-
-print('velocity parameters: ', velparams)
-print('minvel: ', velparams[24:35])
-print('maxvel: ', velparams[48:59])
-print('acc: ', velparams[36:47])
-
-# %%
-# get position counter
-string = ('11 04 01 00 50 01')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-time.sleep(0.04)
-print('bytes in queue: ', s.in_waiting)
-poscount = ''
-while s.in_waiting > 0:
-    poscount += s.read().hex()
-    poscount += ' '
-print('position count: ', poscount)
-
-# %%
-# get relative movement parameter
-string = ('46 04 01 00 50 01')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-time.sleep(0.04)
-print('bytes in queue: ', s.in_waiting)
-relmovparam = ''
-while s.in_waiting > 0:
-    relmovparam += s.read().hex()
-    relmovparam += ' '
-print('relative movement parameter: ', relmovparam)
-
-# %%
-# set relative movement parameter (the last four bytes, inverted somewhat)
-string = ('45 04 06 00 D0 01 01 00 00 70 00 00')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-# %%
-# move relative by predefined parameter
-string = ('48 04 01 00 50 01')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-# %%
-# move relative, setting the parameter
-string = ('48 04 06 00 D0 01 01 00 00 70 00 00')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-# %%
-# get absolute movement parameter
-string = ('51 04 01 00 50 01')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-time.sleep(0.04)
-print('bytes in queue: ', s.in_waiting)
-absmovparam = ''
-while s.in_waiting > 0:
-    absmovparam += s.read().hex()
-    absmovparam += ' '
-print('absolute movement parameter: ', absmovparam)
-
-# %%
-# set absolute movement parameter (the last four bytes, inverted somewhat)
-string = ('50 04 06 00 D0 01 01 00 00 70 00 00')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-# %%
-# move absolute by predefined parameter
-string = ('53 04 01 00 50 01')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-# %%
-# move absolute, setting the parameter
-string = ('53 04 06 00 D0 01 01 00 00 A0 00 00')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-# %%
-# stop
-string = ('65 04 01 00 50 01')
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-time.sleep(0.04)
-stopped = ''
-while s.in_waiting > 0:
-    stopped += s.read().hex()
-    stopped += ' '
-print(stopped)
-
-# %%
-# jog
-string = ('6A 04 01 02 50 01') # 01 02 for forward, 01 01 for reverse rotation
-splitstring = string.split()
-command = [int(str, 16) for str in splitstring]
-print('sending command: ', command)
-s.write(bytes(command))
-
-# %%
-# close stage
-s.close()
-s.is_open
-
+closestage(s)
 
 # %%
 # about the PRM1-Z8:
