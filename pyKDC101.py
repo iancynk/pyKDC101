a773d016807304ac4e48b8b72417d865cceb7927
iancynk <ian.cynk@posteo.eu>
2019-12-06 16:38:35 +0100

    added conversion functions for hex coordinates and angles

--- pyKDC101.py
+++ pyKDC101.py
@@ -34,15 +34,19 @@ commands = {
     "req_genmoveparams":"3B 04 01 00 50 01", # get backlash settings
     "req_homeparams":   "41 04 01 00 50 01", # get home parameters
     "req_moverelparams":"46 04 01 00 50 01", # get rel movement parameters
-    "move_relative":    "48 04 01 00 50 01", # move predefined relative
+    "move_rel":         "48 04 01 00 50 01", # move predefined relative
+    "move_rel_angle":   "48 04 06 00 D0 01 01 00", # move rel by angle (+4 byte)
+    "move_rel_set":     "45 04 06 00 D0 01 01 00", # set relative movement parameter (+4 byte)
     "req_moveabsparams":"51 04 01 00 50 01", # get abs movement parameters
-    "move_absolute":    "53 04 01 00 50 01", # move predefined absolute
+    "move_abs":         "53 04 01 00 50 01", # move predefined absolute
+    "move_abs_angle":   "53 04 06 00 D0 01 01 00", # move predefined absolute (+4 byte)
+    "move_abs_set":     "50 04 06 00 D0 01 01 00", # set absolute movement parameter (+4 byte)
     "move_stop":        "65 04 01 00 50 01", # stop movement
     "req_jogparams":    "17 04 01 00 50 01", # get jog parameters
     "move_jog_forward": "6A 04 01 02 50 01", # jog forward
     "move_jog_backward":"6A 04 01 01 50 01", # jog backward
     "move_velocity_f":  "57 04 01 02 50 01", # move at fixed speed forward
-    "move_velocity_b":  "57 04 01 01 50 01", # move at fixed speed backward
+    "move_velocity_b":  "57 04 01 01 50 01"  # move at fixed speed backward
 }
 
 ################################################################################
@@ -107,7 +111,7 @@ def recvreply(s):
     print('reply: ', reply)
     return reply
 
-
+# %%
 # convert an angle in degree to an increment in the requested binary format
 def convert_angle(angle_degree):
     # convert angle to encoder counts
@@ -120,26 +124,62 @@ def convert_angle(angle_degree):
     # convert to 4 hex numbers (by first converting to int) AND INVERT sequence
     angle_enccnt_hex = ''
     for n in range(4):
-        angle_enccnt_hex = (format(int(angle_enccnt_bin[n*8:n*8+7], 2), '02X')
-                           + ' ' + angle_enccnt_hex)
-    # convert hex stuff to hex string
-    print('Degree', angle_degree, '\n', 'EncCnt', angle_enccnt, '\n',
-          'EncCnt binary', angle_enccnt_bin, '\n', 'EncCnt hex inverted',
-          angle_enccnt_hex)
+        temp = int(angle_enccnt_bin[n*8:n*8+8], 2)
+        angle_enccnt_hex = format(temp, '02X') + ' ' + angle_enccnt_hex
+    # print('Degree', angle_degree, '\n', 'EncCnt', angle_enccnt, '\n',
+    #       'EncCnt binary', angle_enccnt_bin, '\n', 'EncCnt hex inverted',
+    #       angle_enccnt_hex)
+    # add leading whitespace, remove trailing whitespace
+    angle_enccnt_hex = ' ' + angle_enccnt_hex[:-1]
     return(angle_enccnt_hex)
 
-# def move_relative(s, angle):
+# convert an enccnt value from hex to a useable angle
+# e.g. 'DF A2 02 00' = 90 degree
+def convert_enccnt(enccnt):
+    encstring = enccnt.split() # separate to list of hex
+    encint = 0
+    # sum up converted hexvalues times their respective power
+    for n in range(4):
+        encint = encint + (int(encstring[n], 16) * 256**n)
+    angle = round(encint/PRM1_EncCnt, 1)
+    return angle
 
+
+# absolute movement
+def move_abs(s, angle):
+    command = commands["move_abs_angle"] + convert_angle(angle)
+    sendcommand(s, command)
+
+
+#relative movement
+def move_rel(s, angle):
+    command = commands["move_rel_angle"] + convert_angle(angle)
+    sendcommand(s, command)
+
+
+# move home
+def move_home(s):
+    sendcommand(s, commands["move_home"])
 
 ################################################################################
 # CODE
 ################################################################################
 # %%
+# connect stage and let display flash
 s = openstage()
 sendcommand(s, commands["identify"])
 time.sleep(0.1)
 
-angle_hex = convert_angle(180)
+# %%
+# test some commands
+move_abs(s, 13)
+move_rel(s, 10)
+move_home(s)
+
+# %% test conversion functions
+enccnt = convert_angle(90)
+angle = convert_enccnt('DF A2 02 00')
+print('EncCnt:', enccnt, '  Angle:', angle)
 
 # %%
 sendcommand(s, commands["move_stop"])
