1b0df1e169e1b8a9eb026d672596016ab0c730f4
iancynk <ian.cynk@posteo.eu>
2019-12-08 11:27:23 +0100

    improved comments

--- control_stage.py
+++ control_stage.py
@@ -1,4 +1,10 @@
 ################################################################################
+# This code gives basic examples how to use the pyKDC101 library
+# scripted by ian cynk (ian.cynk@posteo.eu) 2019
+################################################################################
+# It doesn't stop being magic just because you know how it works.
+# — Terry Pratchett, The Wee Free Men
+################################################################################
 # IMPORTS
 ################################################################################
 import time
@@ -8,32 +14,41 @@ import pyKDC101 as k
 # CODE
 ################################################################################
 # %%
-# connect stage and let display flash
+# connect stage
 s = k.openstage()
+
+# %%
+# to directly send commands you can use the following command structure
+# let the display flash
 k.sendcommand(s, k.commands["identify"])
 
 # %%
-# test movement commands (can't be interrupted)
-k.move_abs(s, 13)
-k.move_rel(s, 10)
+# to use more comfortable functions, the following functions are implemented
+# movement commands (can't be interrupted)
+k.move_abs(s, 13) # move to 13 degree
+k.move_rel(s, 10) # move plus 10 degree
 k.move_home(s)
 
 # %%
-# test movement commands (interruptable)
+# movement commands (interruptable)
 k.move_abs2(s, 13)
 k.move_rel2(s, 10)
 k.move_home2(s)
 k.stop_move(s)
 
-# %% test conversion functions
+# %%
+# conversion functions to make sense of the hex blobs
 enccnt = k.convert_angle(90)
+print('EncCnt:', enccnt)
 angle = k.convert_enccnt('DF A2 02 00')
-print('EncCnt:', enccnt, '  Angle:', angle)
+print('Angle:', angle)
 
-# %% ask for position
+# %%
+# find current position (encoder being fixed, position could be defined)
 posangle = k.get_pos_angle(s)
 time.sleep(0.5)
 encangle = k.get_enc_angle(s)
 
 # %%
+# disconnect stage
 k.closestage(s)
