0a9eca639e6f234505f7e7dd5bf331d21410580b
iancynk <ian.cynk@posteo.eu>
2019-12-08 11:05:56 +0100

    added interruptible movement commands and debug aids

--- control_stage.py
+++ control_stage.py
@@ -18,25 +18,26 @@ k.sendcommand(s, k.commands["identify"])
 time.sleep(0.1)
 
 # %%
-# test some commands
+# test movement commands (can't be interrupted)
 k.move_abs(s, 13)
 k.move_rel(s, 10)
 k.move_home(s)
-time.sleep(2)
-k.sendcommand(s, k.commands["move_stop"])
+
+# test movement commands (interruptable)
+k.move_abs2(s, 13)
+k.move_rel2(s, 10)
+k.move_home2(s)
+k.stop_move(s)
 
 # %% test conversion functions
 enccnt = k.convert_angle(90)
 angle = k.convert_enccnt('DF A2 02 00')
 print('EncCnt:', enccnt, '  Angle:', angle)
 
-# %%
-k.sendcommand(s, k.commands["req_poscounter"])
-reply = k.recvreply(s)
-k.sendcommand(s, k.commands["req_enccounter"])
-reply = k.recvreply(s)
-reply = '42 04 00 00 01 00'
-a = k.decode_reply(reply)
+# %% ask for position
+posangle = k.get_pos_angle(s)
+time.sleep(0.5)
+encangle = k.get_enc_angle(s)
 
 # %%
 k.closestage(s)
