9c045a2a0b4e15adc7aaf98763366b2a627f7b1b
iancynk <ian.cynk@posteo.eu>
2025-12-03 12:35:51 +0100

    migrated repository

--- src/pyKDC101/__init__.py
+++
@@ -1,8 +0,0 @@
-"""pyKDC101 - Python wrapper to control TL KDC101 controllers"""
-
-import pkg_resources
-__version__ = pkg_resources.require("pyKDC101")[0].version
-__author__ = "iancynk <ian.cynk@posteo.eu>"
-__all__ = ['KDC']
-
-from .core import KDC
